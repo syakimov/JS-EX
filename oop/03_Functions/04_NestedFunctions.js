d81a7805fcbb5fe9c4927e430352b5b421596478
Stefan <stefan.yakimov@tradeo.com>
2016-10-07 17:06:06 +0300

    nested func amendment

--- oop/03_Functions/04_NestedFunctions.js
+++ oop/03_Functions/04_NestedFunctions.js
@@ -1,3 +1,14 @@
 //                          Nested F
 // nested f -> f inside another f
 // na vsqko izvikvane na wrapper f se syzdava nova nested f
+
+function f() {
+  function sum(x, y) {
+    return x + y;
+  }
+  return sum;
+}
+
+console.log(f());         // [Function: sum]
+console.log(typeof f);    // function
+console.log(f() == f());  // false -> because every time it is new f created
