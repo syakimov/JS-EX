b71dc25fcfdc1efc767a45485a957acd68dd2e36
Stefan <stefan.yakimov@tradeo.com>
2016-10-11 18:27:50 +0300

    solved arrow f context mystery

--- questions/arrowDeclarationMystery.js
+++ questions/arrowDeclarationMystery.js
@@ -1,12 +1,20 @@
-// why does this does not work,
-Array.prototype.remove1 = () => {
+//                  solved!
+// Arrow f always have empty obj for context
+Array.prototype.arrowFunction = () => {
+    // always prints {}
     console.log(this);
 };
 
-// but this does?
-Array.prototype.remove2 = function() {
+// anonymous f using function declaration have the
+// obj from which they are called for context(this)
+Array.prototype.anonymousFunction = function() {
     console.log(this);
 };
 
-[1, 2, 3].remove1();
-[1, 2, 3].remove2();
+[1, 2, 3].arrowFunction();     // {}
+[1, 2, 3].anonymousFunction(); // [1, 2, 3]
+
+[].arrowFunction.call([1, 2, 3]);     // {}
+[].anonymousFunction.call([1, 2, 3]); // [1, 2, 3]
+
+// use (_this) => {...} or just use function
